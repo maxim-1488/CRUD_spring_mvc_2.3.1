47da4da7058b577b545683ed149afc5ab0d64020
Maxim <m.krylov88@mail.ru>
2021-03-09 23:29:53 +0300

    Не обновляется юзер

--- src/main/java/ru/maximkrylov/spring_mvc/dao/UserDaoImpl.java
+++ src/main/java/ru/maximkrylov/spring_mvc/dao/UserDaoImpl.java
@@ -40,7 +40,7 @@ public class UserDaoImpl implements UserDao {
     }
 
     @Override
-    //олучаем список всех юзеров
+    //получаем список всех юзеров
     public List<User> getAllUsers() {
         return entityManager.createQuery("from User").getResultList();
     }
